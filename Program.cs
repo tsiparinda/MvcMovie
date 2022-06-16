8ac5342bd1504de5564627e433f4d89bc0421f6c
albatros <tsiparinda@hotmail.com>
2022-06-16 17:07:21 +0300

    rem seeddata

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services = scope.ServiceProvider;
 
-    SeedData.Initialize(services);
-}
+//    SeedData.Initialize(services);
+//}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
