45b03f28c6e72616b63312ea6edddc31b0f49d32
tsipa <tsipa@DESKTOP-QUVFFKB>
2022-06-16 16:07:49 +0300

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            // return "This is my default action...";
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+}
