d997ca42d9a682c1056b3000bd1765cf21ec4f37
albatros <tsiparinda@hotmail.com>
2022-06-23 16:04:04 +0300

    move mvcmovie.data inside solution

---
+++ MvcMovie.Data/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Data.Models
+{
+    public class Movie
+    {
+
+
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+        
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string? Rating { get; set; }
+    }
+}
---
+++ MvcMovie.Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Data.Models.Movie>? Movie { get; set; }
+    }
+}
