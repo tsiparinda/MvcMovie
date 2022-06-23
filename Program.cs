094ffd72b8d364608d9b2589f982ba24b810281f
albatros <tsiparinda@hotmail.com>
2022-06-23 16:25:01 +0300

    rem seed data for run in heroku

--- Program.cs
+++ Program.cs
@@ -16,12 +16,12 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services = scope.ServiceProvider;
 
-    SeedData.Initialize(services);
-}
+//    SeedData.Initialize(services);
+//}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
